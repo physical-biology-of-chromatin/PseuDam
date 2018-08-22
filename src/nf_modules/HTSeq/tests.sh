0d1550a8c5cefb3db35f4d5337f7783cc0d94f90
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-08-22 15:27:58 +0200

    HTSeq: update nf structure

--- src/nf_modules/HTSeq/tests.sh
+++ src/nf_modules/HTSeq/tests.sh
@@ -1,4 +1,4 @@
-nextflow src/nf_modules/HTSeq/tests/counting.nf \
+nextflow src/nf_modules/HTSeq/htseq.nf \
   -c src/nf_modules/HTSeq/htseq.config \
   -profile docker \
   --gtf "data/tiny_dataset/annot/tiny.gff" \
