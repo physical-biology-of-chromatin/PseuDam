2a991dc630a3f180d4745772bb55b13280916802
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2018-08-22 14:19:57 +0200

    BEDtools: update nf structure

--- src/nf_modules/BEDtools/tests.sh
+++ src/nf_modules/BEDtools/tests.sh
@@ -1,5 +1,5 @@
-nextflow src/nf_modules/BEDtools/tests/fasta_from_bed.nf \
-  -c src/nf_modules/BEDtools/bedtools.config \
+nextflow src/nf_modules/BEDtools/fasta_from_bed.nf \
+  -c src/nf_modules/BEDtools/fasta_from_bed.config \
   -profile docker \
   --fasta "data/tiny_dataset/fasta/tiny_v2.fasta" \
   --bed "data/tiny_dataset/annot/tiny.bed" \
