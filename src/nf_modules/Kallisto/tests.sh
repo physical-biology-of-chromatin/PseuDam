c43032e6ca19b76ab902108f30774c690579d116
Laurent Modolo <laurent@modolo.fr>
2018-08-31 16:59:29 +0200

    Kallisto: fix tests.sh

--- src/nf_modules/Kallisto/tests.sh
+++ src/nf_modules/Kallisto/tests.sh
@@ -1,15 +1,15 @@
-nextflow src/nf_modules/Kallisto/indexing.nf \
+./nextflow src/nf_modules/Kallisto/indexing.nf \
   -c src/nf_modules/Kallisto/indexing.config \
   -profile docker \
   --fasta "data/tiny_dataset/fasta/tiny_v2.fasta"
 
-nextflow src/nf_modules/Kallisto/mapping_single.nf \
+./nextflow src/nf_modules/Kallisto/mapping_single.nf \
   -c src/nf_modules/Kallisto/mapping_single.config \
   -profile docker \
   --index "results/mapping/index/tiny_v2.index" \
   --fastq "data/tiny_dataset/fastq/tiny*_S.fastq"
 
-nextflow src/nf_modules/Kallisto/mapping_paired.nf \
+./nextflow src/nf_modules/Kallisto/mapping_paired.nf \
   -c src/nf_modules/Kallisto/mapping_paired.config \
   -profile docker \
   --index "results/mapping/index/tiny_v2.index" \
