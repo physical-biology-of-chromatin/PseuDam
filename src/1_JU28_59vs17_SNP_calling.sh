147563d68de698367f530850c9b79313004d6331
Laurent Modolo <laurent@modolo.fr>
2018-10-03 18:42:41 +0200

    1_JU28_59vs17_SNP_calling.sh: fix tests cmd

--- src/1_JU28_59vs17_SNP_calling.sh
+++ src/1_JU28_59vs17_SNP_calling.sh
@@ -11,7 +11,9 @@ cd ~/projects/JU28_59vs17_SNP/
 
 # training set analysis
 
-./nextflow src/SNP_calling.nf -c src/SNP_calling.config -profile docker --fasta "data/fasta/DBG2OLC-output2.fasta" --fastq "data/samples/*_{1,2}.fastq.gz" -resume -w ~/data/work/ --tumor "[\"NG-10944_JU2859_bis_lib169352_5217_1\"]" --normal "[\"MR_550_clean\", \"MR_350_clean\"]"
+mkdir tests
+cd tests
+../nextflow ../src/SNP_calling.nf -c ../src/SNP_calling.config -profile docker --fasta "../data/fasta/DBG2OLC-output2.fasta" --fastq "../data/samples/*_{1,2}.fastq.gz" -resume -w ~/data/work_s/ --tumor "[\"s_NG-10944_JU2859_bis_lib169352_5217_1\"]" --normal "[\"s_MR_550_clean\", \"s_MR_350_clean\"]"
 ~/scripts/sms.sh "SNP done"
 
 # real set analysis
