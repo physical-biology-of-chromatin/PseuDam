7bf6ea389872d9ec096df6ca84a734eba24f5841
Laurent Modolo <laurent@modolo.fr>
2018-10-08 16:45:29 +0200

    1_JU28_59vs17_SNP_calling.R: add SNP filtering step

--- src/1_JU28_59vs17_SNP_calling.sh
+++ src/1_JU28_59vs17_SNP_calling.sh
@@ -20,10 +20,8 @@ cd tests
 
 ./nextflow src/SNP_calling.nf -c src/SNP_calling.config -profile docker --fasta "data/fasta/DBG2OLC_output2.fasta" --fastq "data/fastq/*_{1,2}.fastq.gz" -resume -w ~/data/work/ --tumor "[\"NG-10944_JU2859_bis_lib169352_5217_1\"]" --normal "[\"MR_550_clean\", \"MR_350_clean\"]"
 ~/scripts/sms.sh "SNP done"
-
-./nextflow src/SNP_calling.nf -c src/SNP_calling.config -profile docker --fasta "data/fasta/DBG2OLC-output2.fasta" --fastq "data/fastq/*_{1,2}.fastq.gz" --sam "results/mapping/sam/*.sam" -resume -w ~/data/work/
-
-./nextflow src/SNP_calling.nf -c src/SNP_calling.config -profile docker --fasta "data/fasta/DBG2OLC-output2.fasta" --fastq "data/fastq/*_{1,2}.fastq.gz" --sam "results/mapping/sam/*.sam" -resume -w ~/data/work/ --tumor "[\"NG-10944_JU2859_bis_lib169352_5217_1\"]" --normal "[\"MR_550_clean\", \"MR_350_clean\"]"
-~/scripts/sms.sh "SNP done"
-
+src/intersect_SNP.R \
+  results/SNP/vcf_samtools/normal_sample_filtered.csv \
+  results/SNP/vcf_samtools/tumor_sample_filtered.csv \
+  results/fasta/DBG2OLC_output2_filtered.fasta
 
