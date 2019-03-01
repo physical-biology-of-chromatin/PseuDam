f1a4275ccf0c456bf5c010d2ddb5c0d3001e0d52
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2019-03-01 15:24:03 +0100

    BEDtools: add singularity to nf files

--- src/nf_modules/BEDtools/tests.sh
+++ src/nf_modules/BEDtools/tests.sh
@@ -3,3 +3,13 @@
   -profile docker \
   --fasta "data/tiny_dataset/fasta/tiny_v2.fasta" \
   --bed "data/tiny_dataset/annot/tiny.bed" \
+  -resume
+
+if [ -x "$(command -v singularity)" ]; then
+./nextflow src/nf_modules/BEDtools/fasta_from_bed.nf \
+  -c src/nf_modules/BEDtools/fasta_from_bed.config \
+  -profile singularity \
+  --fasta "data/tiny_dataset/fasta/tiny_v2.fasta" \
+  --bed "data/tiny_dataset/annot/tiny.bed" \
+  -resume
+fi
