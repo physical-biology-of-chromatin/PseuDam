9fb75825ed07ec22789e4d2b6b13080c7856b761
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2019-03-01 15:17:09 +0100

    UrQt: add singularity tests

--- src/nf_modules/UrQt/tests.sh
+++ src/nf_modules/UrQt/tests.sh
@@ -1,21 +1,25 @@
 ./nextflow src/nf_modules/UrQt/trimming_paired.nf \
   -c src/nf_modules/UrQt/trimming_paired.config \
   -profile docker \
-  --fastq "data/tiny_dataset/fastq/tiny_R{1,2}.fastq"
+  --fastq "data/tiny_dataset/fastq/tiny_R{1,2}.fastq" \
+  -resume
 
 ./nextflow src/nf_modules/UrQt/trimming_single.nf \
   -c src/nf_modules/UrQt/trimming_single.config \
   -profile docker \
-  --fastq "data/tiny_dataset/fastq/tiny_R{1,2}.fastq"
+  --fastq "data/tiny_dataset/fastq/tiny_R{1,2}.fastq" \
+  -resume
 
 if [ -x "$(command -v singularity)" ]; then
 ./nextflow src/nf_modules/UrQt/trimming_single.nf \
   -c src/nf_modules/UrQt/trimming_single.config \
   -profile singularity \
-  --fastq "data/tiny_dataset/fastq/tiny_R{1,2}.fastq"
+  --fastq "data/tiny_dataset/fastq/tiny_R{1,2}.fastq" \
+  -resume
 
 ./nextflow src/nf_modules/UrQt/trimming_single.nf \
   -c src/nf_modules/UrQt/trimming_single.config \
   -profile singularity \
-  --fastq "data/tiny_dataset/fastq/tiny_R{1,2}.fastq"
+  --fastq "data/tiny_dataset/fastq/tiny_R{1,2}.fastq" \
+  -resume
 fi
