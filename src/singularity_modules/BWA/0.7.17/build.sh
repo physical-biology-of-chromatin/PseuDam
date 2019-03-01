3075c9c27e562f0bd342148383cdfccb5bfd3faa
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2019-03-01 16:59:43 +0100

    Bowtie: add singularity files and add signature for singularity build

--- src/singularity_modules/BWA/0.7.17/build.sh
+++ src/singularity_modules/BWA/0.7.17/build.sh
@@ -1,2 +1,3 @@
 #/bin/sh
 sudo singularity build --force bin/bwa:0.7.17.sif src/singularity_modules/BWA/0.7.17/BWA.def
+singularity sign bin/bwa:0.7.17.sif
--- src/singularity_modules/UrQt/d62c1f8/build.sh
+++ src/singularity_modules/UrQt/d62c1f8/build.sh
@@ -1,2 +1,3 @@
 #/bin/sh
 sudo singularity build --force bin/UrQt:d62c1f8.sif src/singularity_modules/UrQt/d62c1f8/UrQt.def
+singularity sign bin/UrQt:d62c1f8.sif
