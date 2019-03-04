2c69d031fbfbf2edcf47f296ec70ab6d6c1c4bc3
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2019-03-04 15:50:01 +0100

    kallisto: add singularity files

---
+++ src/singularity_modules/kallisto/0.43.1/build.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+sudo singularity build --force bin/kallisto:0.43.1.sif src/singularity_modules/kallisto/0.43.1/kallisto.def
+singularity sign bin/kallisto:0.43.1.sif
---
+++ src/singularity_modules/kallisto/0.44.0/build.sh
@@ -0,0 +1,3 @@
+#/bin/sh
+sudo singularity build --force bin/kallisto:0.44.0.sif src/singularity_modules/kallisto/0.44.0/kallisto.def
+singularity sign bin/kallisto:0.44.0.sif
