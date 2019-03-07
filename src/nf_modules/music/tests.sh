cc9f3d11d2bf8d66d3b9b7097b440a151a33b976
Laurent Modolo <laurent@modolo.fr>
2019-03-07 11:35:54 +0100

    music: fix containers and tests

--- src/nf_modules/music/tests.sh
+++ src/nf_modules/music/tests.sh
@@ -7,3 +7,14 @@ cp data/tiny_dataset/map/tiny_v2.sort.bam data/tiny_dataset/map/tiny_v2_control.
   --index "data/tiny_dataset/map/*.sort.bam.bai*" \
   --read_size 50 --frag_size 300 \
   -resume
+
+if [ -x "$(command -v singularity)" ]; then
+./nextflow src/nf_modules/music/peak_calling_single.nf \
+  -c src/nf_modules/music/peak_calling_single.config \
+  -profile singularity \
+  --fasta "data/tiny_dataset/fasta/tiny_v2.fasta" \
+  --bam "data/tiny_dataset/map/*.sort.bam" \
+  --index "data/tiny_dataset/map/*.sort.bam.bai*" \
+  --read_size 50 --frag_size 300 \
+  -resume
+fi
