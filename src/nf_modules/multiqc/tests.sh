6a330a3af86750e4adb6e223aa577eb855377bb6
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2019-05-28 13:39:35 +0200

    multiqc: add missing singulairy test

--- src/nf_modules/multiqc/tests.sh
+++ src/nf_modules/multiqc/tests.sh
@@ -13,13 +13,13 @@
 if [ -x "$(command -v singularity)" ]; then
 ./nextflow src/nf_modules/multiqc/multiqc_paired.nf \
   -c src/nf_modules/multiqc/multiqc_paired.config \
-  -profile docker \
+  -profile singularity \
   --fastq "data/tiny_dataset/fastq/tiny_R{1,2}.fastq" \
   -resume
 
 ./nextflow src/nf_modules/multiqc/multiqc_single.nf \
   -c src/nf_modules/multiqc/multiqc_single.config \
-  -profile docker \
+  -profile singularity \
   --fastq "data/tiny_dataset/fastq/tiny_S.fastq" \
   -resume
 fi
