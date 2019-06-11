b3aee97229d94aa4838f02dda99135b58c4bff80
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2019-06-11 15:16:14 +0200

    subread singularity: fix sps and pbs path

--- src/singularity_modules/subread/1.6.4/build.sh
+++ src/singularity_modules/subread/1.6.4/build.sh
@@ -1,3 +1,3 @@
 #/bin/sh
-sudo singularity build --force bin/subread:1.6.4.img docker://quay.io/biocontainers/subread:1.6.4--h84994c4_1 && \
+sudo singularity build --force bin/subread:1.6.4.img src/singularity_modules/subread/1.6.4/subread.def && \
 singularity sign bin/subread:1.6.4.img
