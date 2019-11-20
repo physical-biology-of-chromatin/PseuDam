a037caea19813089b55f051f60481423f8d385f8
Laurent Modolo <laurent@modolo.fr>
2019-11-20 13:46:45 +0100

    update docker script for v0.4.0

--- src/.docker_modules/docker_push.sh
+++ src/.docker_modules/docker_push.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-fd "Dockerfile" src/docke_modules | perl -pe 's|.*docker_modules/(.*)/(.*)/Dockerfile|\1:\2|g' | awk '{system("docker push lbmc/"$0)}'
+fd "Dockerfile" src/.docker_modules | perl -pe 's|.*docker_modules/(.*)/(.*)/Dockerfile|\1:\2|g' | awk '{system("docker push lbmc/"$0)}'
