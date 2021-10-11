149987de599043cae5eb131cbb97ce77a2cd87ba
Laurent Modolo <laurent.modolo@ens-lyon.fr>
2021-10-11 16:00:06 +0200

    docker: rasusa add cross building for arm

--- src/.docker_modules/rasusa/0.6.0/docker_init.sh
+++ src/.docker_modules/rasusa/0.6.0/docker_init.sh
@@ -2,3 +2,4 @@
 docker pull lbmc/rasusa:0.6.0
 docker build src/.docker_modules/rasusa/0.6.0 -t 'lbmc/rasusa:0.6.0'
 docker push lbmc/rasusa:0.6.0
+docker buildx build --platform linux/amd64,linux/arm64 -t 'lbmc/rasusa:0.6.0' --push src/.docker_modules/rasusa/0.6.0
