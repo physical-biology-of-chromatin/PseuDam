95da402dd266a12694fbbc595612b94b92b2b568
Nathan Lecouvreur <nathan.lecouvreur@ens-lyon.fr>
2022-03-17 16:57:14 +0100

    gtac_finder new version updated ggf and overlap

--- src/.docker_modules/gatc_finder/gatc_finder.py
+++ src/.docker_modules/gatc_finder/gatc_finder.py
@@ -7,16 +7,17 @@ import argparse
 
 #defines qrguments in the command line
 parser = argparse.ArgumentParser()
-parser.add_argument("--genome", action="store");
-args = parser.parse_args();
+parser.add_argument("--genome", action="store")
+parser.add_argument("--overlap", action="store_true")
+args = parser.parse_args()
 
 # Gets the arguments in the command line
 genome_file = args.genome
-
+overlap = args.overlap
 
 # Opening the file to write the positions (bed and GFF)
-f_1 = open("sites.bed", "w")
-f_2 = open("sites.gff", "w")
+f_bed = open("sites.bed", "w")
+f_gff = open("sites.gff", "w")
 
 
 # Motif we are looking for
@@ -52,19 +53,31 @@ for seq_record in SeqIO.parse(genome_file, "fasta"):
 for i in range(2, len(sites_list)):
     
     if sites_list[i-1][0] == sites_list[i][0]:
-
+        
+        # If overlap is specified, uses a bigger overlap
+        if overlap == True:
+            overlap_value = 10
+            
+        # If not sepcified takes only the whole gatc into account
+        elif overlap == False:
+            overlap_value = 4
+        
+        
+        
         bin_chrom = (sites_list[i-1][0])
-        bin_start = (sites_list[i-1][2] - 1)
-        bin_end = (sites_list[i][1] + 1)
+        bin_start = (sites_list[i-1][2] - overlap_value)
+        bin_end = (sites_list[i][1] + overlap_value)
         
         
         # Writes the position in the .bed file (chro/start/end)
-        line_f_1 = f"{bin_chrom}\t{bin_start}\t{bin_end}\n"
-        f_1.write(line_f_1)
+        line_f_gff = f"{bin_chrom}\t{bin_start}\t{bin_end}\n"
+        f_bed.write(line_f_gff)
         
-        
+        # Gets the chromosome name from the file
+        chrom_name = re.search("|(.+?)|", sites_list[i][0]).group(1)
+
         # Writes the same thing but in gff format
-        line_f_2 = f"{chrom}\tNC\texon\t{start_pos}\t{end_pos}\t.\t.\t.\t\n"
-        f_2.write(line_f_2)
+        line_f_gff = f"{chrom_name}\tgatc_finder\tgatc_frag\t{bin_start}\t{bin_end}\t.\t.\t.\n"
+        f_gff.write(line_f_gff)
     
-f_1.close()
+f_bed.close()
